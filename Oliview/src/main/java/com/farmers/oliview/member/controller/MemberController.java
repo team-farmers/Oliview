d6592605ded8deab354412479f5d05614c001f81
Lani Kim <poikl11234@naver.com>
2023-10-28 18:59:04 +0900

    member, myPage 폴더 HTML, CSS, Java 옮기기 & 연결

--- Oliview/src/main/java/com/farmers/oliview/member/controller/MemberController.java
+++ Oliview/src/main/java/com/farmers/oliview/member/controller/MemberController.java
@@ -41,6 +41,40 @@ public class MemberController {
 	}
 	
 	
+	/*--------- 비밀번호 찾기 ---------*/
+
+	// 비밀번호 찾기 첫페이지(정보입력)로 포워드
+	@GetMapping("pw-find")
+	public String pwFind() { 
+		return "member/pw-find";
+	}
+	
+	// 비밀번호 찾기 정보 입력 후 아이디 확인페이지로 포워드
+	@PostMapping("pw-find")
+	public String pwFind(String memberId, String memberName) { 
+		return "member/pw-find-confirm";
+	}
+	
+	// 비밀번호 변경 후 메세지+메인으로 리다이렉트 POST
+	
+	
+	
+	/*--------- 아이디 찾기 ---------*/
+
+	// 아이디 찾기 첫페이지(정보입력)로 포워드
+	@GetMapping("id-find")
+	public String idFind() { 
+		return "member/id-find";
+	}
+	
+	// 아이디 찾기 정보 입력 후 아이디 확인페이지로 포워드
+	@PostMapping("id-find")
+	public String idFind(String memberEmail, String memberName) { 
+		return "member/id-find-confirm";
+	}
+	
+	
+	
 	
 	
 
---
+++ Oliview/src/main/java/com/farmers/oliview/myPage/service/MyPageService.java
@@ -0,0 +1,5 @@
+package com.farmers.oliview.myPage.service;
+
+public interface MyPageService {
+
+}
