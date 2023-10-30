46a04d6f39f3cec7da78977cade8aa50f0c45572
gamza2695 <gamza2695@gmail.com>
2023-10-30 17:42:55 +0900

    1030 yoojin 작업 - ReviewController 셋팅 - Review dto 작성 - result(검색 결과 페이지) html, css, js 작성(미완) - reviewDetail.html 수정

--- Oliview/src/main/java/com/farmers/oliview/review/controller/ReviewController.java
+++ Oliview/src/main/java/com/farmers/oliview/review/controller/ReviewController.java
@@ -16,5 +16,27 @@ import lombok.extern.slf4j.Slf4j;
 public class ReviewController {
 	
 	private final ReviewService service;
+	
+	
+	// 테스트
+	@RequestMapping("result")
+	public String result() {
+		return "review/result";
+	}
+	
+	
+	// 1. 로그인 버튼 -> login.html 포워드
+	
+	// 2. 회원가입 버튼 -> login.html 포워드
+	
+	// 3. 검색 결과 (검색어)
+	
+	// 4. 검색 결과 (닉네임 클릭 시 작성글)
+	
+	// 5. 리뷰 상세 조회 -> reviewDetail 포워드
+	
+	// (인기순, 최신순, 평점순 -> js)
+	// (NEXT 다음페이지 -> js)
+	
 
 }
--- Oliview/src/main/java/com/farmers/oliview/review/model/dto/Review.java
+++ Oliview/src/main/java/com/farmers/oliview/review/model/dto/Review.java
@@ -1,5 +1,25 @@
 package com.farmers.oliview.review.model.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Review {
+	
+	// review 테이블과 매핑되는 필드
+	private int reviewNo;
+	private String reviewTitle;
+	private String reviewAddress;
+	private double starTaste;
+	private double starAmount;
+	private double starClean;
+	private String reviewOneLine;
+	private String reviewImg;
+	private String reviewFood;
+	private String writeDate;
+	private int readCount;
+	private String reviewDelFl;
+	private int memberNo;
 
 }
