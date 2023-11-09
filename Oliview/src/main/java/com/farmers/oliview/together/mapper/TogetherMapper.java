562fe37653e1295cf9e2a03e6b43a4416cd45282
sanng97 <sanng97@naver.com>
2023-11-09 21:16:51 +0900

    boardupdate.html , js 만든후 작성 Togerher 게시글 조회 & 검색 작성

--- Oliview/src/main/java/com/farmers/oliview/together/mapper/TogetherMapper.java
+++ Oliview/src/main/java/com/farmers/oliview/together/mapper/TogetherMapper.java
@@ -1,33 +1,65 @@
 package com.farmers.oliview.together.mapper;
 
 import java.util.List;
+import java.util.Map;
 
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.session.RowBounds;
 
 import com.farmers.oliview.together.dto.Together;
 
+
+
 @Mapper
 public interface TogetherMapper {
-//	
-//	/**
-//	 * 전체 게시글 수 조회
-//	 * @param boardCode
-//	 * @return
-//	 */
-//	int getListCount(int boardCode);
-//	
-//	/**
-//	 * 게시글 목록 조회
-//	 * @param boardCode
-//	 * @param rowBounds
-//	 * @return
-//	 */
-//	List<Together> selectBoardList(int boardCode, RowBounds rowBounds);
+
+	/** 전체 게시글 수 조회
+	 * @param boardCode
+	 * @return listCount
+	 */
+	int getListCount(int boardCode);
+
+	
+	/** 게시글 목록 조회
+	 * @param boardCode
+	 * @param rowBounds 
+	 * @return boardList
+	 */
+	List<Together> selectBoardList(int boardCode, RowBounds rowBounds);
+
+
+	/** 게시글 상세 조회
+	 * @param map
+	 * @return board
+	 */
+	Together boardDetail(Map<String, Object> map);
 	
 	
+	/**
+	 * 조회수 증가
+	 * @param boardNo
+	 * @return
+	 */
+
+	int updateReadCount(int boardNo);
 	
-	
+	/**
+	 *  검색어 일치 게시글 수 조회
+	 * @param paramMap
+	 * @return
+	 */
+
+	int searchListCount(Map<String, Object> paramMap);
+
+	/**
+	 * 검색어 일치 게시글 목록 조회
+	 * @param paramMap
+	 * @param rowBounds
+	 * @return
+	 */
+	List<Together> searchBoardList(Map<String, Object> paramMap, RowBounds rowBounds);
+
+
 	
 	
 }
--- Oliview/src/main/java/com/farmers/oliview/together/service/TogetherServiceImpl.java
+++ Oliview/src/main/java/com/farmers/oliview/together/service/TogetherServiceImpl.java
@@ -21,37 +21,84 @@ public class TogetherServiceImpl implements TogetherService {
 	
 	private final TogetherMapper mapper;
 	
-	
-//	@Override
-//	public Map<String, Object> selectBoardList(int boardCode, int cp) {
-//	
-//		int listCount = mapper.getListCount(boardCode);
-//		
-//		Pagination pagination = new Pagination(cp, listCount);
-//		
-//		int offset = (pagination.getCurrentPage()-1) * pagination.getLimit();
-//		
-//		int limit = pagination.getLimit();
-//		
-//		RowBounds rowBounds = new RowBounds(offset,limit);
-//		
-//		List<Together> boardList = mapper.selectBoardList(boardCode, rowBounds);
-//		
-//		Map<String, Object> map = new HashMap<>();
-//		map.put("boardList", boardList);
-//		map.put("pagination",pagination);
-//		
-//		
-//		
-//		return map;
-//}
-//	
-//	@Override
-//	public Map<String, Object> searchBoardList(Map<String, Object> paramMap, int cp) {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//	
+	/** 게시글 목록 조회
+	    * @param boardCode
+	    * @param cp
+	    * @return
+	    */
+	   @Override
+	   public Map<String, Object> selectBoardList(int boardCode, int cp) {
+	    
+
+	      /* 전체 게시글 수 조회 */
+	      int listCount = mapper.getListCount(boardCode);
+	      
+	      /* cp, listCount를 이용해서 Pagination 객체 생성 */
+	      Pagination pagination = new Pagination(cp, listCount);
+	      
+	      
+	      int offset = (pagination.getCurrentPage()-1) * pagination.getLimit();
+	      
+	      int limit = pagination.getLimit();
+	      
+	      RowBounds rowBounds = new RowBounds(offset, limit);
+	      
+	      
+	      List<Together> boardList = mapper.selectBoardList(boardCode, rowBounds);
+	      
+	      // Map에 담아서 반환
+	      Map<String, Object> map = new HashMap<>();
+	      map.put("boardList", boardList);
+	      map.put("pagination", pagination);
+	      
+	      return map;
+	   }	
+	   	
+	   		/*search*/ 
+			@Override
+			public Map<String, Object> searchBoardList(Map<String, Object> paramMap, int cp) {
+				
+				/* 검색 조건 일치 게시글 수 조회*/
+				int listCount = mapper.searchListCount(paramMap);
+				
+				/* cp, listCount를 이용해서 Pagination 객체 생성*/
+				Pagination pagination = new Pagination(cp, listCount);
+				
+				/* offset은 시작 인덱스 번호를, limit은 조회할 레코드의 개수 */
+				/* RowBounds 객체 생성 */
+				int offset = (pagination.getCurrentPage() -1) * pagination.getLimit();
+				int limit = pagination.getLimit();
+				RowBounds rowBounds = new RowBounds (offset, limit);
+				
+				
+				/* 검색어 일치 게시글 목록 조회 */
+				List<Together> boardList = mapper.searchBoardList(paramMap, rowBounds);
+				
+				
+				/* Map에 담아서 변환 */
+				Map<String, Object>map = new HashMap<>();
+				map.put("boardList", boardList);
+				map.put("pagination", pagination);
+				
+
+				return map;
+			}
+			
+			/* 게시글 상세 조회 + 게시글 댓글 모두 조회 */
+			@Override
+			public Together boardDetail(Map<String, Object> map) {
+			
+				return mapper.boardDetail(map);
+			}
+			
+			/* 조회수 증가 */
+			@Override
+			public int updateReadCount(int boardNo) {
+			
+				return mapper.updateReadCount(boardNo);
+			}
+		
+
 
 }
 
