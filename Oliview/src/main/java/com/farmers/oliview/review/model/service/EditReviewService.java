1be555f9616c9ac6c699e3dbd99b8a0fa7787bdb
freeda <jdb4142@naver.com>
2023-11-09 09:20:18 +0900

    도로명주소 api

--- Oliview/src/main/java/com/farmers/oliview/review/model/service/EditReviewService.java
+++ Oliview/src/main/java/com/farmers/oliview/review/model/service/EditReviewService.java
@@ -2,12 +2,18 @@ package com.farmers.oliview.review.model.service;
 
 import java.util.Map;
 
+import com.farmers.oliview.review.model.dto.Review;
+
 public interface EditReviewService {
 
-	/** 게시글 삭제
-	 * @param paramMap
+	/**
+	 * 게시글 작성
+	 * 
+	 * @param review
 	 * @return
 	 */
-	int deleteReview(Map<String, Object> paramMap);
+	int insertReview(Review review);
+
+	int updateReview(Review review, String deleteOrder);
 
 }
