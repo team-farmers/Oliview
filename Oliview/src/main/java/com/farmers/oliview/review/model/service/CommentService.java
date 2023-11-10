078fd7aba1ac8a46c898f48891836cb542a305b5
gamza2695 <gamza2695@gmail.com>
2023-11-10 17:46:26 +0900

    231110 yoojin 작업2 - 댓글 기능 구현 - 리뷰 상세 조회 프론트엔드 수정

--- Oliview/src/main/java/com/farmers/oliview/review/model/service/CommentService.java
+++ Oliview/src/main/java/com/farmers/oliview/review/model/service/CommentService.java
@@ -12,4 +12,22 @@ public interface CommentService {
 	 */
 	List<Comment> select(int reviewNo);
 
+	/** 댓글 등록
+	 * @param comment
+	 * @return result
+	 */
+	int insert(Comment comment);
+
+	/** 댓글 수정
+	 * @param comment
+	 * @return result
+	 */
+	int update(Comment comment);
+
+	/** 댓글 삭제
+	 * @param commentNo
+	 * @return result
+	 */
+	int delete(int commentNo);
+
 }
