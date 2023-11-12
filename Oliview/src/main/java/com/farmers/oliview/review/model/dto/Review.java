741220c6494c9714c1b7a2728085a5603bb3fb3b
gamza2695 <gamza2695@gmail.com>
2023-11-12 21:48:35 +0900

    231112 yoojin 작업 - 최신순, 평점순 조회 백엔드 + 프론트엔드(진행중) - 닉네임 클릭 시 게시글 검색 - 로그인 후 작성글 조회 시 수정,삭제 버튼 활성화

--- Oliview/src/main/java/com/farmers/oliview/review/model/dto/Review.java
+++ Oliview/src/main/java/com/farmers/oliview/review/model/dto/Review.java
@@ -37,5 +37,10 @@ public class Review {
     
     // 가게평점
     private double storeStarTotal;
+    
+    
+    // 평점순
+    private int storeCount;
+    private double storeStarTotalRate;
 
 }
--- Oliview/src/main/resources/static/js/review/result.js
+++ Oliview/src/main/resources/static/js/review/result.js
@@ -32,6 +32,12 @@ sortPopular.addEventListener("click", () => {
   sortRating.classList.remove("clicked");
 
 
+  const params = new URL(location.href).searchParams;
+  const searchInput1 = params.get("searchInput"); // 검색어
+
+  location.href = "/review/searchReview?searchInput=" + searchInput1
+  
+
 });
 
 
@@ -41,9 +47,87 @@ sortPopular.addEventListener("click", () => {
 
 sortLatest.addEventListener("click", () => {
 
-  sortPopular.classList.remove("clicked");
+  sortPopular.classList.add("unclicked");
   sortLatest.classList.add("clicked");
   sortRating.classList.remove("clicked");
+
+  const params = new URL(location.href).searchParams;
+  const searchInput2 = params.get("searchInput"); // 검색어
+
+  // fetch("/review/searchReview?" + `searchInput=${searchWord}`)
+  fetch("/review/sortLatest?searchInput="+searchInput2)
+
+  .then(response => response.json())
+  .then((reviewList) => {
+    console.log(reviewList);
+
+    /* 리뷰, 페이지네이션 지우기 */
+    const reviewFull = document.getElementById("reviewFull");
+    reviewFull.innerHTML = "";
+    const reviewPagination = document.getElementById("reviewPagination");
+    reviewPagination.innerHTML = "";
+
+    
+    /* 리뷰 리스트 가져오기 */
+    for(let review of reviewList){
+      
+      /* 열 생성 */
+      const reviewRow = document.createElement("section");
+      reviewRow.classList.add("review-row");
+
+      /* 리뷰 아티클 생성 */
+      const reviewArticle = document.createElement("article");
+      reviewArticle.classList.add("review-col");
+
+      /* 리뷰 a태그, 이미지 태그 생성 */
+      const aImg = document.createElement("a");
+      const Img = document.createElement("img");
+      aImg.classList.add("review-img");
+      aImg.href="/review/" + review.reviewNo;
+      Img.src=review.reviewImg;
+
+      /* 가게명 생성 */
+      const divTitle = document.createElement("div");
+      const aTitle = document.createElement("a");
+      divTitle.classList.add("reviewTitle");
+      aTitle.href="/review/" + review.reviewNo;
+      aTitle.innerHTML=review.reviewTitle;
+
+      /* 닉네임,평점,작성일 생성 */
+      const divNSD = document.createElement("div");
+      const aNickname = document.createElement("a");
+      const sectionStar = document.createElement("section");
+      const spanI = document.createElement("span");
+      const sectionDate = document.createElement("section");
+
+      divNSD.classList.add("nickname-star-date");
+      aNickname.classList.add("nickname");
+      aNickname.innerHTML=review.memberNickname;
+      aNickname.href="/review/searchReview?searchInput=" + review.memberNickname;
+      sectionStar.classList.add("star");
+      sectionStar.innerHTML=(review.starTotal).toFixed(1);
+      spanI.innerHTML=`<i class="fa-sharp fa-solid fa-star fa-ml" style="color: #f1c93b;"></i>`;
+      sectionDate.classList.add("writeDate");
+      sectionDate.innerHTML=review.writeDate;
+
+
+
+      /* 정렬! */
+      divNSD.append(aNickname, sectionStar, spanI, sectionDate)
+      divTitle.append(aTitle);
+      aImg.append(Img);
+      reviewArticle.append(aImg, divTitle, divNSD);
+      reviewRow.append(reviewArticle);
+      reviewFull.append(reviewRow);
+
+      console.log("확인");
+
+
+    }
+
+
+  })
+  .catch(e => console.log(e));
 
 });
 
@@ -53,10 +137,86 @@ sortLatest.addEventListener("click", () => {
 
 sortRating.addEventListener("click", () => {
 
-  sortPopular.classList.remove("clicked");
+  sortPopular.classList.add("unclicked");
   sortLatest.classList.remove("clicked");
   sortRating.classList.add("clicked");
 
+  const params = new URL(location.href).searchParams;
+  const searchInput3 = params.get("searchInput"); // 검색어
+
+  fetch("/review/sortRating?searchInput="+searchInput3)
+
+  .then(response => response.json())
+  .then((reviewList) => {
+    console.log(reviewList);
+
+    /* 리뷰, 페이지네이션 지우기 */
+    const reviewFull = document.getElementById("reviewFull");
+    reviewFull.innerHTML = "";
+    const reviewPagination = document.getElementById("reviewPagination");
+    reviewPagination.innerHTML = "";
+
+    let rank = 1;
+    
+    /* 리뷰 리스트 가져오기 */
+    for(let review of reviewList){
+
   
-  location.href="searchReview?searchInput=" + params.get("searchInput");
+      
+      /* 열 생성 */
+      const reviewRow = document.createElement("section");
+      reviewRow.classList.add("review-row");
+
+      /* 리뷰 아티클 생성 */
+      const reviewArticle = document.createElement("article");
+      reviewArticle.classList.add("review-col");
+
+      /* 리뷰 a태그, 이미지 태그 생성 */
+      const aImg = document.createElement("a");
+      const Img = document.createElement("img");
+      aImg.classList.add("review-img");
+      aImg.href="/review/reviewTitle?=" + review.reviewTitle;
+      // Img.src=
+
+
+      /* 랭킹 생성 */
+      const divRank = document.createElement("div");
+      divRank.classList.add("reviewRank")
+      const sectionRankNo = document.createElement("section");
+      const sectionTitle = document.createElement("section");
+      sectionRankNo.innerHTML="#" + rank;
+      rank++;
+      sectionTitle.innerHTML = review.reviewTitle;
+
+
+      /* 평점, 인원 생성 */
+      const divStar = document.createElement("div");
+      const sectionStar = document.createElement("section");
+      const spanI = document.createElement("span");
+      const sectionCount = document.createElement("section")
+
+      divStar.classList.add("star-count");
+      sectionStar.classList.add("star");
+      sectionStar.innerHTML=(review.storeStarTotalRate).toFixed(1);
+      spanI.innerHTML=`<i class="fa-sharp fa-solid fa-star fa-ml" style="color: #f1c93b;"></i>`;
+      sectionCount.classList.add("starCount");
+      sectionCount.innerHTML="(" + review.storeCount + "명)";
+
+
+      /* 정렬! */
+      divStar.append(sectionStar, spanI, sectionCount)
+      divRank.append(sectionRankNo, sectionTitle);
+      aImg.append(Img);
+      reviewArticle.append(aImg, divRank, divStar);
+      reviewRow.append(reviewArticle);
+      reviewFull.append(reviewRow);
+
+      console.log("확인");
+
+
+    }
+
+
+  })
+  .catch(e => console.log(e));
 });
