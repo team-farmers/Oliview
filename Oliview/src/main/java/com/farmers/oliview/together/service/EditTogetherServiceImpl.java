bef814bb28220cb8228206fe79fb046ae98a0678
sanng97 <sanng97@naver.com>
2023-11-13 14:02:27 +0900

    together mapper 오류해결 및 이미지 저장 완료 & css 버튼 구현완료

--- Oliview/src/main/java/com/farmers/oliview/together/service/EditTogetherServiceImpl.java
+++ Oliview/src/main/java/com/farmers/oliview/together/service/EditTogetherServiceImpl.java
@@ -35,189 +35,137 @@ public class EditTogetherServiceImpl implements EditTogetherService {
 	
 	
 	@Value("${my.together.webpath}")
-		private String webPath;
+	private String webPath;
 	
 	@Value("${my.together.location}")
 	private String folderPath;
 	
 	// 게시글 작성
 	@Override
-	public int insertBoard(Together together, List<MultipartFile> images) throws IllegalStateException, IOException {
+	public int insertBoard(Together together, MultipartFile img) throws IllegalStateException, IOException {
 		
-		
+		String rename = Util.fileRename(img.getOriginalFilename());
+		together.setBoardImg(webPath+rename);
+				
 		int result = mapper.insertBoard(together);
 		
-		
-		int boardNo = together.getBoardNo();
-		
-		List<BoardImg> uploadList = new ArrayList<>();
-		
-		for(int i = 0; i <images.size() ; i++) {
+		int boardNo = 0;
+		if(result == 1) {
+			boardNo = together.getBoardNo();
 			
-		
-			if(images.get(i).getSize() > 0) {
-				
-				BoardImg img = new BoardImg();
-				
-				img.setBoardNo(boardNo);
-				img.setImgOrder(i); 
-				
-				img.setImgOriginalName(images.get(i).getOriginalFilename()); 
-				
-				
-				
-				img.setImgPath(webPath);
-				
-			
-				img.setImgRename(Util.fileRename(images.get(i).getOriginalFilename()));
-				
-				
-				img.setUploadFile(images.get(i));
-				
-				
-			
-				uploadList.add(img);
-				
-			}
-		}  
-		
-		if(uploadList.isEmpty()) {
-			
-			return boardNo;
+			img.transferTo(new File(folderPath + rename));
 		}
-		
-		
-
-		result = mapper.insertUploadList(uploadList);
-		
-
-		if(result == uploadList.size()) {
 			
-			
-			
-			for(BoardImg img : uploadList) {
-				
-				img.getUploadFile().transferTo(new File(folderPath + img.getImgRename()));
-				
-			}
-			
-		}else { 
-			
-			throw new RuntimeException("파일 정보 DB 삽입 실패");
-			
-		}
-
-			return boardNo;
+		return boardNo;
+	}
 	
-		}
-	
-		//게시글 삭제
-		@Override
-		public int deleteBoard(Map<String, Integer> paramMap) {
-			
-			return mapper.deleteBoard(paramMap);
-		}
-	
+	//게시글 삭제
+	@Override
+	public int deleteBoard(Map<String, Integer> paramMap) {
 		
-		//게시글 수정
-		@Override
-		public int updateBoard(Together together, List<MultipartFile> images, String deleteOrder) throws IllegalStateException, IOException {
-				
-				int result = mapper.updateBoard(together);
-				
-				if(result == 0) 
-					return 0;
-				
-				
-				/*deleteOrder의 내용이 존재하면 삭제 수행*/
+		return mapper.deleteBoard(paramMap);
+	}
 
-					if(!deleteOrder.equals("")) {
-						
-						Map <String,Object> map = new HashMap<>();
-						
-						map.put("boardNo",together.getBoardNo());
-						map.put("deleteOrder", "deleteOrder");
-						
-						result = mapper.imageDelete(map);
-						
-						if(result == 0) {
-							throw new BoardUpdateException("이미지 삭제 실패");
-						}
+	
+	//게시글 수정
+	@Override
+	public int updateBoard(Together together, List<MultipartFile> images, String deleteOrder) throws IllegalStateException, IOException {
+			
+			int result = mapper.updateBoard(together);
+			
+			if(result == 0) 
+				return 0;
+			
+			
+			/*deleteOrder의 내용이 존재하면 삭제 수행*/
 
+				if(!deleteOrder.equals("")) {
+					
+					Map <String,Object> map = new HashMap<>();
+					
+					map.put("boardNo",together.getBoardNo());
+					map.put("deleteOrder", "deleteOrder");
+					
+					result = mapper.imageDelete(map);
+					
+					if(result == 0) {
+						throw new BoardUpdateException("이미지 삭제 실패");
 					}
-			
+
+				}
 		
-						List<BoardImg> uploadList = new ArrayList<>();
+	
+					List<BoardImg> uploadList = new ArrayList<>();
+					
+					for(int i=0 ; i<images.size() ; i++) {
 						
-						for(int i=0 ; i<images.size() ; i++) {
+						if(images.get(i).getSize() > 0) {
 							
-							if(images.get(i).getSize() > 0) {
-								
-								BoardImg img = new BoardImg();
-								
-								
-								img.setBoardNo(together.getBoardNo()); /*몇번 게시글의 이미지*/
-								img.setImgOrder(i); /* 몇 번째 이미지*/
-								
-								
-								img.setImgOriginalName(images.get(i).getOriginalFilename()); /* 원본 파일명 */
-								
-								
-								img.setImgPath(webPath); /*웹 접근 경로*/
-								
-								img.setImgRename(Util.fileRename(images.get(i).getOriginalFilename())); /* 변경된 파일명 */
-								
-								img.setUploadFile(images.get(i));
-								
-								uploadList.add(img);
-								
-								
-								result = mapper.updateBoardImg(img); /* 있다 -> 변경 */
-								
-								
-								if(result == 0) {
-									mapper.boardImgInsert(img); /* 없다 -> 추가 */
-								}
-								
-								
-							}		
+							BoardImg img = new BoardImg();
 							
 							
+							img.setBoardNo(together.getBoardNo()); /*몇번 게시글의 이미지*/
+							img.setImgOrder(i); /* 몇 번째 이미지*/
 							
-						}
 							
-						/* upload list에 있는 이미지를 서버에 저장*/
-						if( !uploadList.isEmpty() ) {
-							result = 1;
+							img.setImgOriginalName(images.get(i).getOriginalFilename()); /* 원본 파일명 */
 							
-							for(BoardImg img : uploadList) {
-								
-								img.getUploadFile().transferTo(new File( folderPath + img.getImgRename() ) );
+							
+							img.setImgPath(webPath); /*웹 접근 경로*/
+							
+							img.setImgRename(Util.fileRename(images.get(i).getOriginalFilename())); /* 변경된 파일명 */
+							
+							img.setUploadFile(images.get(i));
+							
+							uploadList.add(img);
+							
+							
+							result = mapper.updateBoardImg(img); /* 있다 -> 변경 */
+							
+							
+							if(result == 0) {
+								mapper.boardImgInsert(img); /* 없다 -> 추가 */
 							}
-						}
-					
-						return result;
+							
+							
+						}		
+						
+						
+						
 					}
-					
-			
-			
-			
-		}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+						
+					/* upload list에 있는 이미지를 서버에 저장*/
+					if( !uploadList.isEmpty() ) {
+						result = 1;
+						
+						for(BoardImg img : uploadList) {
+							
+							img.getUploadFile().transferTo(new File( folderPath + img.getImgRename() ) );
+						}
+					}
+				
+					return result;
+				}
+				
+		
+		
+		
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
 	
 	
