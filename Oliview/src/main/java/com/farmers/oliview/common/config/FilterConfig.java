f2c53bc4f34c2903f8323e6b6715f1474314949a
lani <poikl11234@naver.com>
2023-11-13 11:41:54 +0900

    231113 yerina_ 로그인 전/로그인후/관리자권한 필터 생성(완료)

---
+++ Oliview/src/main/java/com/farmers/oliview/common/config/FilterConfig.java
@@ -0,0 +1,89 @@
+package com.farmers.oliview.common.config;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.farmers.oliview.common.filter.AdminFilter;
+import com.farmers.oliview.common.filter.LoginFilter;
+import com.farmers.oliview.common.filter.LogoutFilter;
+
+@Configuration
+public class FilterConfig {
+
+	/**
+	 * 로그인 필터 세팅
+	 * 
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean<LoginFilter> loginFilter() {
+
+		FilterRegistrationBean<LoginFilter> filter = new FilterRegistrationBean<>();
+		filter.setFilter(new LoginFilter()); // 사용할 로그인 필터 객체 추가
+
+		// 로그인 전) 마이페이지, 리뷰/같이먹어요 글쓰기/수정/채팅 못들어가게 필터링함
+
+		String[] filteringUrl = { "/myPage/*", "/together/boardUpdate", "/together/chat", "/together/posting",
+				"/together/talk", "/review/insert", "/review/update" };
+		
+		// 필터링할 주소 지정
+		filter.setUrlPatterns(Arrays.asList(filteringUrl));
+		filter.setName("loginFilter");
+		filter.setOrder(1);
+
+		return filter;
+	}
+
+	
+	/** 로그아웃 필터 세팅
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean<LogoutFilter> logoutFilter(){
+	 
+		FilterRegistrationBean<LogoutFilter> filter = new FilterRegistrationBean<>();
+		filter.setFilter(new LogoutFilter()); // 사용할 로그인 필터 객체 추가
+		 
+		// 로그인 후) 로그인, 회원가입 페이지, 비밀번호 찾기, 아이디찾기 못들어가게 필터링 함
+		List<String> filteringUrl = new ArrayList<>();
+		 
+		filteringUrl.add("/member/*");
+		filter.setUrlPatterns(filteringUrl);
+		filter.setName("logoutFilter");
+		filter.setOrder(1);
+
+		return filter;
+	}
+	
+	
+	/** 관리자권한 필터 세팅
+	 * @return
+	 */
+	@Bean
+	public FilterRegistrationBean<AdminFilter> adminFilter(){
+		 
+		FilterRegistrationBean<AdminFilter> filter = new FilterRegistrationBean<>();
+		filter.setFilter(new AdminFilter()); // 사용할 로그인 필터 객체 추가
+		 
+		// 로그인 후) 로그인, 회원가입 페이지, 비밀번호 찾기, 아이디찾기 못들어가게 필터링 함
+		List<String> filteringUrl = new ArrayList<>();
+		 
+		filteringUrl.add("/admin/*");
+		filter.setUrlPatterns(filteringUrl);
+		filter.setName("adminFilter");
+		filter.setOrder(2);
+
+		return filter;
+	}
+	
+	
+	
+	
+	 
+
+}
--- Oliview/src/main/java/com/farmers/oliview/main/controller/MainController.java
+++ Oliview/src/main/java/com/farmers/oliview/main/controller/MainController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.farmers.oliview.main.service.MainService;
 import com.farmers.oliview.review.model.dto.Review;
@@ -39,6 +41,40 @@ public class MainController {
 		
 		return "common/main";
 	}
+	
+	
+	/** 비로그인 상태에서 마이페이지/글쓰기 접근 시
+	 * @param ra
+	 * @return
+	 */
+	@GetMapping("loginError")
+	public String loginError(RedirectAttributes ra) {
+		ra.addFlashAttribute("message", "로그인 후 이용해주세요");
+		return "redirect:/";
+	}
+	
+	
+	/** 로그인 상태에서 회원가입/로그인 페이지 접근 시
+	 * @param ra
+	 * @return
+	 */
+	@GetMapping("logoutError")
+	public String logoutError(RedirectAttributes ra) {
+		ra.addFlashAttribute("message", "정상적이지 않은 접근입니다.");
+		return "redirect:/";
+	}
+	
+	
+	/** 관리자권한 없는 경우 관리자페이지 접근 시
+	 * @param ra
+	 * @return
+	 */
+	@GetMapping("adminError")
+	public String adminError(RedirectAttributes ra) {
+		ra.addFlashAttribute("message", "정상적이지 않은 접근입니다.");
+		return "redirect:/";
+	}
+	
 	
 	
 	
