c1b88ae810ef80c046e11e70c74c608c0d40aa26
lani <poikl11234@naver.com>
2023-11-16 12:42:16 +0900

    231116 yerina_ 같이먹어요 필터처리, 메인페이지 글쓰기 버튼 위치수정

--- Oliview/src/main/java/com/farmers/oliview/main/controller/MainController.java
+++ Oliview/src/main/java/com/farmers/oliview/main/controller/MainController.java
@@ -50,7 +50,7 @@ public class MainController {
 	@GetMapping("loginError")
 	public String loginError(RedirectAttributes ra) {
 		ra.addFlashAttribute("message", "로그인 후 이용해주세요");
-		return "redirect:/";
+		return "redirect:/member/login";
 	}
 	
 	
